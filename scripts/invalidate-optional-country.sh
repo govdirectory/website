a415df591241ae657abe9a822a95c7e3db1483f3
Abbe98 <albin.post@gmail.com>
2021-10-12 15:53:40 +0200

    🚧 add a script to update optional  account data per country

---
+++ scripts/invalidate-optional-country.sh
@@ -0,0 +1,14 @@
+#/bin/sh
+
+if [[ $1 == "" ]]
+  then
+  echo "Missing the country argument."
+  exit 0
+fi
+
+for i in $(find site/$1/* -type d);
+  do
+  qid=$(echo ${i%%/} | cut -f3 -d"/");
+  echo $qid
+  snowman cache organization-optional.rq $qid --invalidate
+done
