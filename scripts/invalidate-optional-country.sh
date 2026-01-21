5f19cd04208d8f4dfafad837d2f75df219a5bc1c
Abbe98 <albin.post@gmail.com>
2026-01-21 21:21:24 +0100

    scripts: update cache scripts for Snowman 0.7+

--- scripts/invalidate-optional-country.sh
+++ scripts/invalidate-optional-country.sh
@@ -10,5 +10,5 @@ for i in $(find site/$1/* -type d);
   do
   qid=$(echo ${i%%/} | cut -f3 -d"/");
   echo $qid
-  snowman cache organization-optional.rq $qid --invalidate
+  snowman cache sparql clear organization-optional.rq $qid
 done
